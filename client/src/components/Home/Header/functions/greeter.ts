41be62409180e2fcc96e2abe33f65a5ccde2862b
Leonard Menzel <dev@leomenzel.de>
2022-09-04 19:22:57 +0200

    change greeter times

--- client/src/components/Home/Header/functions/greeter.ts
+++ client/src/components/Home/Header/functions/greeter.ts
@@ -1,18 +1,12 @@
 export const greeter = (): string => {
   const now = new Date().getHours();
   let msg: string;
-/*
-  const greetingsSchemaRaw =
-    localStorage.getItem('greetingsSchema') ||
-    'Good evening!;Good afternoon!;Good morning!;Good night!';
-  const greetingsSchema = greetingsSchemaRaw.split(';');
-*/
+
   if (now >= 18) msg = 'header.greeter.evening';
   else if (now == 12) msg = 'header.greeter.noon';
-  else if (now >= 12) msg = 'header.greeter.day';
+  else if (now >= 10) msg = 'header.greeter.day';
   else if (now >= 6) msg = 'header.greeter.morning';
-  else if (now >= 0) msg = 'header.greeter.night';
-  else msg = 'header.greeter.generic';
+  else msg = 'header.greeter.night';
 
   return msg;
 };
